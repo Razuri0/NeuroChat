78dada915bde5af38dd2f8288b701eade65525fb
Razuri0 <david.laurent@gmx.de>
2025-10-10 00:53:08 +0200

    this will surely work

--- utils/mistral_framework.py
+++ utils/mistral_framework.py
@@ -5,9 +5,8 @@ def mistral_completion(api, messages, model="mistral-small-2407", max_tokens=100
     client = Mistral(api_key=api)
     response = client.chat.completions.create(
         model=model,
-        messages=[
-            messages
-        ],
+        messages=
+        messages,
         max_tokens=max_tokens
     )
     return response.choices[0].message['content']
