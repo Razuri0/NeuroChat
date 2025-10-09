219bb841ebd2f5f6efb57b31e88965d220d39b50
Razuri0 <david.laurent@gmx.de>
2025-10-10 01:10:15 +0200

    debug stuff

--- utils/mistral_framework.py
+++ utils/mistral_framework.py
@@ -3,6 +3,7 @@ from mistralai import Mistral
 
 def mistral_completion(api, messages, model="mistral-small-2407", max_tokens=100):
     client = Mistral(api_key=api)
+    print(messages)
     response = client.chat.complete(
         model=model,
         messages=messages,
