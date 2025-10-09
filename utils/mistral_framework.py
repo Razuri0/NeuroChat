cbfc3b7bc05c89a48412fdf7d782f7cf73a954a8
Razuri0 <david.laurent@gmx.de>
2025-10-10 00:59:20 +0200

    copilot can't do nothing frfr on god nr.2

--- utils/mistral_framework.py
+++ utils/mistral_framework.py
@@ -3,10 +3,9 @@ from mistralai import Mistral
 
 def mistral_completion(api, messages, model="mistral-small-2407", max_tokens=100):
     client = Mistral(api_key=api)
-    response = client.chat.completions.create(
+    response = client.chat.complete(
         model=model,
-        messages=
-        messages,
+        messages=messages,
         max_tokens=max_tokens
     )
     return response.choices[0].message.content
