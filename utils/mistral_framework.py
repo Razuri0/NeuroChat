de5adc36d437756baf752c8ea10b3c5fade4e032
Razuri0 <david.laurent@gmx.de>
2025-10-10 00:54:14 +0200

    this will surely work nr.2

--- utils/mistral_framework.py
+++ utils/mistral_framework.py
@@ -10,14 +10,3 @@ def mistral_completion(api, messages, model="mistral-small-2407", max_tokens=100
         max_tokens=max_tokens
     )
     return response.choices[0].message['content']
-
-
-def main():
-    with open("../mistral_key", "r") as file:
-        mistral_key = file.read().strip()
-    prompt = "test"
-    messages = [{"role": "user", "content": prompt}]
-    response = mistral_completion(messages=messages, api=mistral_key)
-    print(f'Mistral: {response}\n')
-
-main()
