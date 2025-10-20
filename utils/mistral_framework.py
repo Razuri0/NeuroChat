f79ca6f84887a08384e7c759c5b994bebae1ef27
Razuri0 <david.laurent@gmx.de>
2025-10-20 21:49:07 +0200

    finally it works

--- utils/mistral_framework.py
+++ utils/mistral_framework.py
@@ -2,10 +2,11 @@ import os
 from mistralai import Mistral
 
 def mistral_completion(api, messages, model="mistral-small-latest", max_tokens=100, pre_prompt=""):
+    prompt = pre_prompt + messages
     client = Mistral(api_key=api)
     response = client.chat.complete(
         model=model,
-        messages=pre_prompt + messages,
+        messages=prompt,
         max_tokens=max_tokens
     )
     return response.choices[0].message.content
